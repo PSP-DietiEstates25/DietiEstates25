38af7ef4ee00ac6479297475f84e915a20d9afb7
valentina piscopo <valentina.piscopo4@studenti.unina.it>
2025-05-31 00:21:20 +0200

    dashboard agente minimal
    attualmente contiene solo il menù

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,16 +1,28 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { AgentDashboardComponent } from './features/agent-dashboard/agent-dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  
   {
-  path: 'history',
-  loadComponent: () =>
-    import('./features/history/history.component').then(m => m.HistoryComponent)
+    path: 'history',
+    loadComponent: () =>
+      import('./features/history/history.component').then(
+        (m) => m.HistoryComponent
+      ),
   },
+
   {
-  path: 'history',
-  loadComponent: () =>
-    import('./features/history/history.component').then(m => m.HistoryComponent)
-}
+    path: 'history',
+    loadComponent: () =>
+      import('./features/history/history.component').then(
+        (m) => m.HistoryComponent
+      ),
+  },
+
+  {
+    path: 'agent',
+    component: AgentDashboardComponent,
+  },
 ];
