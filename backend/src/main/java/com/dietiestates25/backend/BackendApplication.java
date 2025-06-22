f2d8332c73f6bcaaca431fe8efa8dd3f170ffec6
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2025-06-22 20:10:13 +0200

    aggiunta lombok

--- backend/src/main/java/com/dietiestates25/backend/BackendApplication.java
+++ backend/src/main/java/com/dietiestates25/backend/BackendApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-/*import com.example.accessingdatajpa.Customer;
-import com.example.accessingdatajpa.CustomerRepository;*/
+import com.dietiestates25.backend.model.Esempio;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -18,39 +17,19 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
- /* 
+	
 	@Bean
-	public CommandLineRunner demo(CustomerRepository repository) {
+	public CommandLineRunner demo() {
 		return (args) -> {
-	      // save a few customers
-	      repository.save(new Customer("Jack", "Bauer"));
-	      repository.save(new Customer("Chloe", "O'Brian"));
-	      repository.save(new Customer("Kim", "Bauer"));
-	      repository.save(new Customer("David", "Palmer"));
-	      repository.save(new Customer("Michelle", "Dessler"));
-
-	      // fetch all customers
-	      log.info("Customers found with findAll():");
-	      log.info("-------------------------------");
-	      repository.findAll().forEach(customer -> {
-	        log.info(customer.toString());
-	      });
-	      log.info("");
-
-	      // fetch an individual customer by ID
-	      Customer customer = repository.findById(1L);
-	      log.info("Customer found with findById(1L):");
-	      log.info("--------------------------------");
-	      log.info(customer.toString());
-	      log.info("");
-
-	      // fetch customers by last name
-	      log.info("Customer found with findByLastName('Bauer'):");
-	      log.info("--------------------------------------------");
-	      repository.findByLastName("Bauer").forEach(bauer -> {
-	        log.info(bauer.toString());
-	      });
-	      log.info("");
-	    };
-	  } */
+			Esempio e1 = new Esempio();
+			Esempio e2 = new Esempio("ciao", "bello");
+			
+			log.info("----------ESEMPIO DEMONSTRATION----------");
+			log.info(e1.toString());
+			log.info(e2.toString());
+			e1.setName("luca");
+			e1.setPassword("miapass");
+			log.info(e1.toString());
+		};
+	}
 }
---
+++ backend/src/main/java/com/dietiestates25/backend/model/Esempio.java
@@ -0,0 +1,19 @@
+package com.dietiestates25.backend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Esempio {
+	
+	private String name;
+	private String password;
+	
+}
