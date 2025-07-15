2821a4f04fe503a3ea92adf887a80078a88ec42c
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2025-07-15 19:12:51 +0200

    modifiche per compatibilita' mappa google

--- code/frontend/src/app/map/map.component.ts
+++ code/frontend/src/app/map/map.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
+
 
 @Component({
-  selector: 'app-map2',
+  selector: 'app-map',
   imports: [],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
 export class MapComponent {
+
 
 }
