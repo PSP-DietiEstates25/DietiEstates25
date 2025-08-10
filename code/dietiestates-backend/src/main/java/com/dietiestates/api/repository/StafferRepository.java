d6a91ed9fb57f90e840a7c922bafc1ced5893c13
valentina piscopo <valentina.piscopo4@studenti.unina.it>
2025-08-10 17:53:39 +0200

    Create StafferRepository.java

---
+++ code/dietiestates-backend/src/main/java/com/dietiestates/api/repository/StafferRepository.java
@@ -0,0 +1,7 @@
+package com.dietiestates.api.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.dietiestates.api.model.Staffer;
+
+public interface StafferRepository extends CrudRepository<Staffer, String> {
+}
