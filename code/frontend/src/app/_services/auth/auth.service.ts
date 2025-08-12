90ff79d19a3f8f69fc1b5810046764705f035157
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2025-08-12 16:37:34 +0200

    cambiamenti

--- code/frontend/src/app/_services/auth/auth.service.ts
+++ code/frontend/src/app/_services/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, inject, signal, WritableSignal, computed, effect } from '@angular/core';
 import { LocalStorageService } from '../local-storage/local-storage.service';
 import { AuthState } from '../../interfaces/auth/auth-state';
+import { jwtDecode } from 'jwt-decode';
 
 /*
 
@@ -11,11 +12,32 @@ export class AuthService {
   
   localStorageService = inject(LocalStorageService);
 
-  authState: WriteableSignal<AuthState> = signal<AuthState>({
+  authState: WritableSignal<AuthState> = signal<AuthState>({
     email: this.getEmail(),
     token: this.getToken(),
-    isAuthenticated: this.isAuthenticated(),
+    isAuthenticated: this.isUserAuthenticated(),
   });
+
+  username = computed(() => this.authState().email);
+  token = computed(() => this.authState().token);
+  isAuthenticated = computed(() => this.authState().isAuthenticated);
+
+  constructor(){
+    effect( () => {
+      const token = this.authState().token;
+      const email = this.authState().email;
+      if(token !== null){
+        this.localStorageService.setItem("token", token);
+      } else {
+        this.localStorageService.removeItem("token");
+      }
+      if(email !== null){
+        this.localStorageService.setItem("email", email);
+      } else {
+        this.localStorageService.removeItem("email");
+      }
+    });
+  }
 
   getEmail(){
     return this.localStorageService.getItem('email');
@@ -25,5 +47,42 @@ export class AuthService {
     return this.localStorageService.getItem('token');
   }
 
-  isAuthenticated(){}
+  isUserAuthenticated(): boolean {
+    return this.verifyToken(this.getToken());
+  }
+
+  async updateToken(token: string) {
+    const decodedToken: any = jwtDecode(token);
+    const email = decodedToken.email;
+    this.authState.set({
+      email: email,
+      token: token,
+      isAuthenticated: this.verifyToken(token)
+    })
+  }
+
+  verifyToken(token: string | null): boolean {
+    if(token !== null){
+      try{
+        const decodedToken = jwtDecode(token);
+        const expiration = decodedToken.exp;
+        if(expiration === undefined || Date.now() >= expiration * 1000){
+          return false;
+        } else {
+          return true;
+        }
+      } catch(error) {
+        return false;
+      }
+    }
+    return false;
+  }
+
+  logout(){
+    this.authState.set({
+      email: null,
+      token: null,
+      isAuthenticated: false
+    });
+  }
 }
