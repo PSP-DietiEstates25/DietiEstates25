6b1042ddfd33c72bcc9e727c5c050ccc92233a2a
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2025-09-13 10:07:57 +0200

    POST admin completamente funzionante (unico da verificare estateagent)
    - Disponibilità admin globale con seguenti credenziali:

--- code/dietiestates-backend/src/main/java/com/dietiestates/api/DietiestatesBackendApplication.java
+++ code/dietiestates-backend/src/main/java/com/dietiestates/api/DietiestatesBackendApplication.java
@@ -1,12 +1,18 @@
 package com.dietiestates.api;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.dietiestates.api.model.Admin;
 import com.dietiestates.api.model.Role;
+import com.dietiestates.api.repository.AdminRepository;
 import com.dietiestates.api.repository.RoleRepository;
 
 @SpringBootApplication
@@ -19,8 +25,12 @@ public class DietiestatesBackendApplication {
 
 	//command line runner per la creazione di un user
 	@Bean
-	public CommandLineRunner runner(RoleRepository roleRepository) {
-		return _ -> {
+	public CommandLineRunner runner(
+			RoleRepository roleRepository,
+			AdminRepository adminRepository,
+			PasswordEncoder passwordEncoder
+			) {
+		return _ -> {		
 			if(roleRepository.findByName("USER").isEmpty()) {
 				roleRepository.save(
 						Role.builder().name("USER").build()
@@ -37,6 +47,18 @@ public class DietiestatesBackendApplication {
 				roleRepository.save(
 						Role.builder().name("ESTATE_AGENT").build()
 						);
+			}
+			
+			if(adminRepository.findByEmail("admin@admin.com").isEmpty()) {
+				var admin = new Admin();
+				admin.setCreatedDate(LocalDateTime.now());
+				admin.setEmail("admin@admin.com");
+				admin.setPassword(passwordEncoder.encode("adminpassword"));
+				admin.setAccountLocked(false);
+				admin.setEnabled(true);
+				admin.setRoles(List.of(roleRepository.findByName("ADMIN").get()));
+				admin.setAdmin(admin);
+				adminRepository.save(admin);
 			}
 		};
 	}
--- code/dietiestates-backend/src/main/java/com/dietiestates/api/model/Admin.java
+++ code/dietiestates-backend/src/main/java/com/dietiestates/api/model/Admin.java
@@ -31,6 +31,7 @@ public class Admin extends User {
 	
 	@ManyToOne
 	@JoinColumn(
+			nullable = false,
 			name = "admin_id",
 			foreignKey = @ForeignKey(name = "ADMIN_ADMIN_ID_FK")
 			)
