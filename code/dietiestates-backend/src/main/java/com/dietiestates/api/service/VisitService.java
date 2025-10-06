411402d5f8837bcfe56cf6b56c3f34c92f4bf118
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2025-10-06 09:42:14 +0200

    Aggiornamento service visit

--- code/dietiestates-backend/src/main/java/com/dietiestates/api/service/VisitService.java
+++ code/dietiestates-backend/src/main/java/com/dietiestates/api/service/VisitService.java
@@ -1,8 +1,16 @@
 package com.dietiestates.api.service;
 
 import com.dietiestates.api.dto.request.VisitRequest;
+import com.dietiestates.api.dto.response.VisitResponse;
+import com.dietiestates.api.exception.notowned.VisitNotOwnedByRealEstateException;
 
 public interface VisitService {
 
 	void createVisit(VisitRequest request, Long realEstateId);
+	
+	VisitResponse getVisitById(
+			Long realEstateId,
+			Long visitId
+			)
+		throws VisitNotOwnedByRealEstateException;
 }
--- code/dietiestates-backend/src/main/java/com/dietiestates/api/serviceImpl/VisitServiceImpl.java
+++ code/dietiestates-backend/src/main/java/com/dietiestates/api/serviceImpl/VisitServiceImpl.java
@@ -3,6 +3,8 @@ package com.dietiestates.api.serviceImpl;
 import org.springframework.stereotype.Service;
 
 import com.dietiestates.api.dto.request.VisitRequest;
+import com.dietiestates.api.dto.response.VisitResponse;
+import com.dietiestates.api.exception.notowned.VisitNotOwnedByRealEstateException;
 import com.dietiestates.api.factory.VisitFactory;
 import com.dietiestates.api.finder.RealEstateFinder;
 import com.dietiestates.api.finder.UserFinder;
@@ -10,6 +12,7 @@ import com.dietiestates.api.finder.VisitFinder;
 import com.dietiestates.api.mapper.VisitMapper;
 import com.dietiestates.api.repository.VisitRepository;
 import com.dietiestates.api.service.VisitService;
+import com.dietiestates.api.verifier.VisitVerifier;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +23,7 @@ public class VisitServiceImpl implements VisitService {
 	private final VisitRepository visitRepository;
 	private final VisitFactory visitFactory;
 	private final VisitFinder visitFinder;
+	private final VisitVerifier visitVerifier;
 	private final VisitMapper visitMapper;
 	
 	private final RealEstateFinder realEstateFinder;
@@ -35,6 +39,20 @@ public class VisitServiceImpl implements VisitService {
 		
 		var visit = visitFactory.createVisitFromSpec(visitSpec, user, realEstate);
 		visitRepository.save(visit);
+	}
+
+	@Override
+	public VisitResponse getVisitById(
+			Long realEstateId,
+			Long visitId
+			)
+			throws VisitNotOwnedByRealEstateException {
+		var realEstate = realEstateFinder.getRealEstateById(realEstateId);
+		var visit = visitFinder.getVisitById(visitId);
+		
+		visitVerifier.checkVisitOwnedByRealEstate(visit.getRealEstate().getId(), realEstate.getId());
+		
+		return visitMapper.fromEntity(visit);
 	}
 
 	/*
