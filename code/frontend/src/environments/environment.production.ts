c4901ca40a047636c42520ee818bc49deadcdd37
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2025-12-25 20:30:52 +0100

    dovrebbe funzionare

---
+++ code/frontend/src/environments/environment.production.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: '/api',
+  loginUrl: '/oauth2/authorization/messaging-client-oidc?prompt=login',
+  geoapifyAPIKey: 'd6ef1142975941368b3831ce8487681b',
+  apiKeyParam: 'apiKey=',
+  placesRadius: 500,
+  placesLimit: 20,
+  searchFooterMaxVisiblePages: 5,
+  offerFooterMaxVisiblePages: 5,
+};
