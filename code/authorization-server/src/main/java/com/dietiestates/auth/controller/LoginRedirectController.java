0590cc1e377b80ea64ac3cd12a3c36f087831fb7
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2025-12-27 00:25:20 +0100

    fr

--- code/authorization-server/src/main/java/com/dietiestates/auth/controller/LoginRedirectController.java
+++ code/authorization-server/src/main/java/com/dietiestates/auth/controller/LoginRedirectController.java
@@ -17,7 +17,6 @@ public class LoginRedirectController implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("redirect:" + properties.loginUrl());
         registry.addViewController(properties.loginUrl()).setViewName("forward:/auth/index.html");
     }
 }
