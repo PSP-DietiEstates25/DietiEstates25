e6a3365df7592326bf62625e01d6ca618c494265
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2025-12-28 20:14:06 +0100

    altro

--- code/authorization-server/src/main/java/com/dietiestates/auth/config/SecurityConfig.java
+++ code/authorization-server/src/main/java/com/dietiestates/auth/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
         http
                 .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(properties.registerUrl(), properties.csrfUrl())
+                        .ignoringRequestMatchers(properties.registerUrl(), properties.csrfUrl(), "/api/account/**")
                         .csrfTokenRepository(csrfRepository)
                         .csrfTokenRequestHandler(csrfHandler)
                 )
