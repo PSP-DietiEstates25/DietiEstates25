d7b7793ca8bc8deb2306a01088f16ccbd9299842
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2025-12-30 15:18:02 +0100

    test per email agente dentro admin real estate list

--- code/frontend/src/app/interfaces/full-real-estate.ts
+++ code/frontend/src/app/interfaces/full-real-estate.ts
@@ -1,12 +1,17 @@
 import { AdCategory } from '../enums/ad-category.enum';
 import { EnergyClass } from '../enums/energy-class.enum';
 import {
+  CadastralDataResponse,
   GeographicalPositionResponse,
   RealEstate,
+  RealEstateResponse,
   UtilityResponse,
 } from '../services/models';
 
-export interface FullRealEstate extends RealEstate {
+export interface FullRealEstate extends RealEstateResponse {
+  //aggiunta che può funzionare
+  cadastralData: CadastralDataResponse,
+  //
   geographicalPosition: GeographicalPositionResponse;
   utility: UtilityResponse;
   energyClass?: EnergyClass;
